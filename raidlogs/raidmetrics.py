8c68a45f226ac56a7f8e96eb172b32334fd771e6
Vazquez, Jose J <jose.vazquez2@thermofisher.com>
2020-03-02 17:20:29 -0800

    udpate raidmetrics to use new fflogs api

--- raidlogs/raidmetrics.py
+++ raidlogs/raidmetrics.py
@@ -1,5 +1,11 @@
-from raidlogs import requestlib
-from . import config
+from raidlogs import config
+from raidlogs import fflogs
 
-def getFightReport(report_id):
-    pass
+
+def get_fight_report(report_id):
+    api = fflogs.Api(config.API_KEY)
+    report = api.get_reports(report_id)
+    print(report)
+
+
+get_fight_report("WxadmGXqDRNCBvZf")
