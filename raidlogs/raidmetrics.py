14a501eb5e990c5b33c8fa73d0036592f8fd7099
Vazquez, Jose J <jose.vazquez2@thermofisher.com>
2020-03-03 15:21:13 -0800

    Add basic implementation of raid metrics, this will be the outmost layer of the app that intercats with the users

--- raidlogs/raidmetrics.py
+++ raidlogs/raidmetrics.py
@@ -4,8 +4,8 @@ from raidlogs import fflogs
 
 def get_fight_report(report_id):
     api = fflogs.Api(config.API_KEY)
-    report = api.get_reports(report_id)
-    print(report)
+    report_data = api.get_fight_report(report_id)
+    return report_data
 
 
 get_fight_report("WxadmGXqDRNCBvZf")
---
+++ tests/test_raidmetrics.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestRaidMetrics(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_fight_report_simple_pass(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
