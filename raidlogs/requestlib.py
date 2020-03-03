8a8986ff352247275500b814d5d5caf75f28bd63
Vazquez, Jose J <jose.vazquez2@thermofisher.com>
2020-03-02 16:57:37 -0800

    Renamed requestlib

--- raidlogs/requestlib.py
+++ raidlogs/requestlib.py
@@ -1,10 +1,17 @@
+import urllib.response
 import urllib.request
 import urllib.error
+import urllib.parse
 
 
-def get(url):
+def get(url, api_key):
     try:
-        with urllib.request.urlopen(url) as response:
+        params = {'api_key': api_key}
+        encoded_query = urllib.parse.urlencode(params)
+
+        full_url = url + "?%s" % encoded_query
+
+        with urllib.request.urlopen(full_url) as response:
             data = response.read()
             encoding = response.headers.get_content_charset()
             return data.decode(encoding)
