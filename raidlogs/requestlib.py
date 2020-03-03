28dcf4b932fb9a581aa84defcab344402f789421
Vazquez, Jose J <jose.vazquez2@thermofisher.com>
2020-03-02 17:01:21 -0800

    remove spaces, and unused imports

--- raidlogs/requestlib.py
+++ raidlogs/requestlib.py
@@ -1,20 +1,19 @@
-import urllib.response
 import urllib.request
 import urllib.error
 import urllib.parse
 
 
-def get(url, api_key):
+def get(url):
     try:
-        params = {'api_key': api_key}
-        encoded_query = urllib.parse.urlencode(params)
-
-        full_url = url + "?%s" % encoded_query
-
-        with urllib.request.urlopen(full_url) as response:
+        with urllib.request.urlopen(url) as response:
             data = response.read()
             encoding = response.headers.get_content_charset()
             return data.decode(encoding)
 
     except urllib.error.HTTPError as er:
         print(er)
+
+
+def encode_url_with_params(url, params):
+    encoded_query = urllib.parse.urlencode(params)
+    return url + "?%s" % encoded_query
