2a719330d8336c50ae644ffa4bc97f1e480e942d
Vazquez, Jose J <jose.vazquez2@thermofisher.com>
2020-03-02 17:02:50 -0800

    Remove unused code, now we will access the api using the fflogs api

--- raidlogs/raidmetrics.py
+++ raidlogs/raidmetrics.py
@@ -1,9 +1,5 @@
-from raidlogs import requestLib
+from raidlogs import requestlib
 from . import config
 
-FFLogsReportsURL = "https://www.fflogs.com:443/v1/report/fights/"
-
-
 def getFightReport(report_id):
-    resp = requestLib.get(FFLogsReportsURL + "%s" % report_id, config.API_KEY)
-    return resp
+    pass
