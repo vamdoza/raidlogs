b847c2a847f541b8eb8911404c0a7177bef0aa3f
Vazquez, Jose J <jose.vazquez2@thermofisher.com>
2020-03-03 13:18:24 -0800

    Move json_response_to_dict to request lib, and update tests

--- raidlogs/requestlib.py
+++ raidlogs/requestlib.py
@@ -1,6 +1,7 @@
 import urllib.request
 import urllib.error
 import urllib.parse
+import json
 
 
 def get(url):
@@ -12,8 +13,15 @@ def get(url):
 
     except urllib.error.HTTPError as er:
         print(er)
+        return json.dumps({"status": er.code, "error": er.reason})
 
 
 def encode_url_with_params(url, params):
     encoded_query = urllib.parse.urlencode(params)
     return url + "?%s" % encoded_query
+
+
+def response_to_dict(resp):
+    if resp is None:
+        return {}
+    return json.loads(resp, encoding='utf-8')
--- tests/unit/test_requestlib.py
+++ tests/unit/test_requestlib.py
@@ -4,30 +4,28 @@ import unittest.mock
 from raidlogs import requestlib
 
 
+# TODO mock request lib to get a canned response.
 class TestRequestLib(unittest.TestCase):
 
-    def test_getWithNoValidURLRaisesValueError(self):
+    def test_get_with_no_valid_URL_raises_value_error(self):
         with self.assertRaises(ValueError):
             requestlib.get("")
 
-    def test_getWithNoValidURLRaisesValueError(self):
-        with self.assertRaises(ValueError):
-            requestlib.get("python.com")
-
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_getWithNonExistingURLReturns404(self, mock_print):
+    def test_get_with_non_existing_path_prints_http_error(self, mock_print):
         requestlib.get("https://www.google.com/non-existing-path")
         self.assertEqual(mock_print.getvalue(), 'HTTP Error 404: Not Found\n')
 
-    def test_getReturnDataWhenCorrectURL(self):
-        response = requestlib.get("https://www.google.com/")
-        self.assertIsNotNone(response)
-
-    def test_EncodeURLWithParamsReturnEncodedURL(self):
-        url = "www.google.com"
-        params = {'user_id': 12345678910}
+    def test_encode_url_with_params_return_encoded_URL(self):
+        url = "https://www.fflogs.com:443/v1"
+        params = {'api_key': 12345678910}
         encoded_url = requestlib.encode_url_with_params(url, params)
-        self.assertEqual(encoded_url, "www.google.com?user_id=12345678910")
+        self.assertEqual(encoded_url, "https://www.fflogs.com:443/v1?api_key=12345678910")
+
+    def test_response_to_dict_returns_dict_from_json(self):
+        json = "{}"
+        dictionary = requestlib.response_to_dict(json)
+        self.assertIsNotNone(dictionary)
 
 
 if __name__ == '__main__':
